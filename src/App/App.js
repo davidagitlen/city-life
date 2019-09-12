f8269a083bfe16cac0e771855e0a22b81da84bcc
davidagitlen <davidagitlen@gmail.com>
2019-09-12 09:54:29 -0600

    Add npm packages and basic configuration for enzyme/jest, redux, router

---
+++ src/App/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link, BrowserRouter } from 'react-router-dom';
+
+
+class App extends Component{
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore } from 'redux';
+const store = createStore(rootReducer, composeWithDevTools())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
