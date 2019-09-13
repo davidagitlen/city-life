c498a6a4c165c70a31bf7236264d429074fb6d38
davidagitlen <davidagitlen@gmail.com>
2019-09-13 10:34:30 -0600

    Add fetch for images and structure reducer to handle all data

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,15 @@ export const setCityScores = (ordinal, array) => ({
   ordinal,
   array
 });
+
+export const setCityDetails = (ordinal, details) => ({
+  type: 'SET_CITY_DETAILS',
+  ordinal,
+  details
+});
+
+export const setCityImages = (ordinal, images) => ({
+  type: 'SET_CITY_IMAGES',
+  ordinal,
+  images
+})
