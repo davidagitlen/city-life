bde1188493a060ac0c1bc76263c5400223992b94
davidagitlen <davidagitlen@gmail.com>
2019-09-16 20:17:48 -0600

    Add tests for CityForm, up until mapStateToProps and mapDispatchToProps

--- src/containers/CityForm/CityForm.test.js
+++ src/containers/CityForm/CityForm.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { CityForm, mapStateToProps, mapDispatchToProps } from './CityForm';
 import { setCityScores, setcityDetails, setCityImages } from '../../actions';
 import { fetchUrbanAreas, fetchCityScores, findAdditionalData, fetchAdditionalData, fetchCityImages } from '../../util/apiCalls';
-import { formatCityName, formatAdditionalCityData } from '../../util/dataCleaner'; 
 import { shallow } from 'enzyme';
 
 jest.mock('../../util/apiCalls');
@@ -68,11 +67,30 @@ describe('CityForm', () => {
     });
 
     fetchAdditionalData.mockImplementation(() => {
-      return Promise.resolve({});
+      return Promise.resolve({
+        population: 100000,
+        location: {
+          latlon: {
+            latitude: 100,
+            longitude: 100
+          }
+        },
+        full_name: 'Dorberord',
+        _links: {
+          'city:timezone' : {
+            name: 'Dorberord'
+          },
+          'city:country': {
+            name: 'Divad'
+          }
+        }
+      });
     });
 
     fetchCityImages.mockImplementation(() => {
-      return Promise.resolve({});
+      return Promise.resolve({
+        photos : [{attribution: 'Flickr', image: 'http://blah.com/blah'}]
+      });
     });
 
     wrapper = shallow(
@@ -166,7 +184,7 @@ describe('CityForm', () => {
       expect(fetchCityScores).toHaveBeenCalledWith(`https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/`);
     });
 
-    it('should invoke setCityScores if fetch is successful and response is ok', async () => {
+    it('should invoke setCityScores with the correct arguments if fetch is successful and response is ok', async () => {
 
       const wrapper = shallow(
         <CityForm
@@ -176,19 +194,132 @@ describe('CityForm', () => {
         />
       ); 
       
-      wrapper.instance().getCityScores();
-
-      expect(setCityScores).toHaveBeenCalled();
-
-
-
-
-
+      await wrapper.instance().getCityScores();
+      expect(wrapper.instance().props.setCityScores).toHaveBeenCalledWith('one', [{ color: 'blue', name: 'Score', score_out_of_10: 10 }]);
     });
 
+    it.skip('should set error property of state to error message if fetchCityScores is not successful', async () => {
+      const wrapper = shallow(
+        <CityForm
+          cityInfo={mockCityInfo}
+          ordinal='one'
+          setCityScores={jest.fn()}
+        />
+      );
+      
+      fetchCityScores.mockImplementation(() => {
+        return Promise.reject(Error('Sorry, it didn\'t work'));
+      });
+      
+      const expected = 'Sorry, it didn\'t work';
+
+      await wrapper.instance().getCityScores();
+      console.log(wrapper.instance().state)
+      expect(wrapper.instance().state.error).toEqual(expected);
+    });
   });
 
+  describe('getCityImages', () => {
 
+    it('should invoke fetchCityImages with a properly formatted city name taken from state and cleaned with replace', () => {
+      const mockEvent = {
+        target: {
+          name: 'city',
+          value: 'San, Francisco, Bay, Area'
+        }
+      };
 
-  
+      const wrapper = shallow(
+        <CityForm
+          cityInfo={mockCityInfo}
+          ordinal='one'
+        />
+      );
+
+      wrapper.instance().handleCitySelection(mockEvent);
+      wrapper.instance().getCityImages();
+      expect(fetchCityImages).toHaveBeenCalledWith(`https://api.teleport.org/api/urban_areas/slug:san-francisco-bay-area/`);
+    });
+
+    it('should invoke setCityImages with the correct arguments if fetch is successful and response is ok', async () => {
+
+      const wrapper = shallow(
+        <CityForm
+          cityInfo={mockCityInfo}
+          ordinal='one'
+          setCityImages={jest.fn()}
+        />
+      );
+
+      await wrapper.instance().getCityImages();
+      expect(wrapper.instance().props.setCityImages).toHaveBeenCalledWith('one', { attribution: 'Flickr', images: 'http://blah.com/blah' });
+    });
+  });
+
+  describe('getAdditionalCityData', () => {
+
+    it('should invoke findAdditionalData with a properly formatted city name taken from state and cleaned and the original city name', () => {
+
+      const mockEvent = {
+        target: {
+          name: 'city',
+          value: 'San Francisco Bay Area'
+        }
+      };
+
+      const wrapper = shallow(
+        <CityForm
+          cityInfo={mockCityInfo}
+          ordinal='one'
+        />
+      );
+
+      wrapper.instance().handleCitySelection(mockEvent);
+      wrapper.instance().getAdditionalCityData();
+      expect(findAdditionalData).toHaveBeenCalledWith('San Francisco', 'San Francisco Bay Area');
+    });
+  });
+
+  describe('handleAdditionalData', () => {
+
+    it('should call fetchAdditionalData with the url returned from getAdditionalCityData', async () => {
+      const mockGetAdditionalCityData = jest.fn().mockImplementation(() => 'http://blurgh.com')
+
+      const wrapper = shallow(
+        <CityForm
+          cityInfo={mockCityInfo}
+          ordinal='one'
+        />
+      );
+
+      wrapper.instance().getAdditionalCityData = mockGetAdditionalCityData;
+      wrapper.instance().forceUpdate();
+      await wrapper.instance().handleAdditionalData();
+      expect(fetchAdditionalData).toHaveBeenCalledWith('http://blurgh.com');
+    });
+
+    it('should invoke setCityDetails with the correct arguments if fetch is successful and response is ok', async () => {
+
+      const wrapper = shallow(
+        <CityForm
+          cityInfo={mockCityInfo}
+          ordinal='one'
+          setCityDetails={jest.fn()}
+        />
+      );
+
+      const expected = {
+        population: '100,000',
+        latitude: 100,
+        longitude: 100,
+        fullName: 'Dorberord',
+        timeZone: 'Dorberord',
+        country: 'Divad'
+      }
+
+      await wrapper.instance().handleAdditionalData();
+      expect(wrapper.instance().props.setCityDetails).toHaveBeenCalledWith('one', expected);
+    });
+  });
+
 });
